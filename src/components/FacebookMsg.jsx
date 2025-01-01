8deccf2cadb960df169266445143b877a90e71f3
solaymanisharify <solayman.isharify@gmail.com>
2025-01-01 17:31:53 +0600

    com

--- src/components/FacebookMsg.jsx
+++ src/components/FacebookMsg.jsx
@@ -1,31 +1,10 @@
-import { useEffect } from "react";
+import { FacebookProvider, CustomChat } from "react-facebook";
 
 const FacebookMsg = () => {
-  useEffect(() => {
-    const fbScript = document.getElementById("facebook-jssdk");
-    if (!fbScript) {
-      const script = document.createElement("script");
-      script.id = "facebook-jssdk";
-      script.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-      script.async = true;
-      document.body.appendChild(script);
-    } else {
-      window.FB && window.FB.XFBML.parse();
-    }
-  }, []);
-
   return (
-    <>
-      <div id="fb-root"></div>
-      <div
-        className="fb-customerchat"
-        attribution="setup_tool"
-        page_id="100401268725508" // Replace with your Facebook Page ID
-        theme_color="#0084FF" // Customize as needed
-        logged_in_greeting="Hi! How can we help you?"
-        logged_out_greeting="Hi! Please log in to chat with us."
-      ></div>
-    </>
+    <FacebookProvider appId="498324479379955" chatSupport>
+      <CustomChat pageId="100401268725508" minimized={true} />
+    </FacebookProvider>
   );
 };
 
