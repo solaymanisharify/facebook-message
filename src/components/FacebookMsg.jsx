40f35e74fbd1a84b2b5f5beb1c9fb3f3ea9527dd
solaymanisharify <solayman.isharify@gmail.com>
2025-01-01 15:55:40 +0600

    com

--- src/components/FacebookMsg.jsx
+++ src/components/FacebookMsg.jsx
@@ -1,42 +1,10 @@
-import { useEffect } from "react";
+import { FacebookProvider, CustomChat } from "react-facebook";
 
 const FacebookMsg = () => {
-  useEffect(() => {
-    // Load the Facebook SDK script dynamically
-    const loadFacebookSDK = () => {
-      if (document.getElementById("facebook-jssdk")) return;
-      const script = document.createElement("script");
-      script.id = "facebook-jssdk";
-      script.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-      document.body.appendChild(script);
-    };
-
-    // Initialize Facebook SDK
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: "498324479379955", // Your App ID
-        autoLogAppEvents: true,
-        xfbml: true,
-        version: "v15.0",
-      });
-    };
-
-    loadFacebookSDK();
-  }, []);
-
   return (
-    <>
-      {/* Your Messenger Chat Plugin */}
-      <div id="fb-root"></div>
-      <div
-        className="fb-customerchat"
-        attribution="setup_tool"
-        page_id="100401268725508"
-        theme_color="#0084ff"
-        logged_in_greeting="Hi! How can we help you?"
-        logged_out_greeting="Hi! How can we help you?"
-      ></div>
-    </>
+    <FacebookProvider appId="498324479379955" chatSupport>
+      <CustomChat pageId="100401268725508" minimized={true} />
+    </FacebookProvider>
   );
 };
 
